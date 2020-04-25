64a02982ab5ee61d5ab8477385859fc1859e84b5
Catrina Friday <cat.friday@gmail.com>
2020-04-25 00:11:19 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,4 +1,4 @@
-`def introduction(name)
+def introduction(name)
   puts "Hi, my name is #{name}."
 end
 introduction("name")
@@ -6,7 +6,7 @@ introduction("name")
 def introduction_with_language_optional (name, language="Ruby")
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
-introduction_with_language ("name")`
+introduction_with_language ("name")
 
 
 
